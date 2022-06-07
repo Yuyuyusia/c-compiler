a4f813b904960024b7c1d59793311c3b3bd014a2
YusiaLee <94372458+Yuyuyusia@users.noreply.github.com>
2022-06-07 12:22:22 +0800

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-mkdir final
-cd final
-echo "password" | sudo -S apt-get install git build-essential flex bison llvm clang
-git clone https://github.com/gjlmotea/Compiler-sample
-cd ..
-cp makefile ./final/Compiler-sample
-cd final/Compiler-sample
-make all
